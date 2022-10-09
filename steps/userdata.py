eb2d3fac565f03277b2fdbc5148ccfdf62ba1047
Claudio Klingler <ck@realtime-projects.com>
2022-10-09 11:26:03 +0200

    optimize formatting.

--- steps/userdata.py
+++ steps/userdata.py
@@ -4,5 +4,4 @@ from radish import then, world
 @then("I ensure '{variable}' is '{value}'")
 def i_ensure_variable_is(step, variable, value):
     datavalue = world.config.user_data[variable]
-    assert datavalue == value, \
-        f"mismatch for {variable}: {datavalue} != {value}"
+    assert datavalue == value, f"mismatch for {variable}: {datavalue} != {value}"
